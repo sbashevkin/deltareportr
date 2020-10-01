098c1da715d34348990f24020268ac3d305ce8f9
sbashevkin <sam.bashevkin@deltacouncil.ca.gov>
2020-10-01 14:09:30 -0700

    Added version of the report with all seasons

--- docs/Vignettes.R
+++ docs/Vignettes.R
@@ -1,3 +1,5 @@
 devtools::build_vignettes()
 Report_year=2019
 file.copy(file.path("doc", "Delta_Smelt_conditions_report.html"), file.path("docs", paste0("Delta_Smelt_conditions_report_",Report_year, ".html")), overwrite = TRUE)
+
+file.copy(file.path("doc", "Delta_Smelt_conditions_report_full.html"), file.path("docs", paste0("Delta_Smelt_conditions_report_full_",Report_year, ".html")), overwrite = TRUE)
